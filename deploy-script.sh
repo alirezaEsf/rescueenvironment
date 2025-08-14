b4f262316d0944b54fb05f6af452b882bb6ae56c
alirezaES <alireza.esfandiar77@gmail.com>
2025-08-14 19:49:08 +0330

    init deploy.yml

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Postbuild script: No files to copy. Skipping config deployment."
+# کپی index.html به عنوان 404.html
+cp dist/rescueenvironment/index.html dist/rescueenvironment/404.html
+
+echo "Postbuild: 404.html created for Angular route refresh support on GitHub Pages"
