ce58cf32c2ac2f5589f9f779ba8199fec68ae607
alirezaES <alireza.esfandiar77@gmail.com>
2025-08-14 19:50:33 +0330

    CHANGE deploy.yml

--- deploy-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# کپی index.html به عنوان 404.html
-cp dist/rescueenvironment/index.html dist/rescueenvironment/404.html
-
-echo "Postbuild: 404.html created for Angular route refresh support on GitHub Pages"
