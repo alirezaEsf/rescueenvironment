6682736b138a67a52e99298115e445d6b4318fe8
alirezaES <alireza.esfandiar77@gmail.com>
2025-08-14 17:49:42 +0330

    init deploy.yml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ import { APP_BASE_HREF } from '@angular/common';
     providers:[MessageService,ToastService,
         {
             provide: APP_BASE_HREF,
-            useValue: environment.production ? '/gateway-ui/' : '/'
+            useValue: environment.production ? '/rescueenvironment/' : '/'
         }
     ],
     imports: [RouterOutlet, MainComponent],
